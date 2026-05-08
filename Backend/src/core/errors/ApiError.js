24f57afe6bac358dc2b1bbf66f37fd291a0efa48
bodilypat <139784676+bodilypat@users.noreply.github.com>
2026-05-08 13:49:16 -0700

    Implement ApiError class for error handling
    Fix constructor call and capture stack trace.

---
+++ Backend/src/core/errors/ApiError.js
@@ -0,0 +1,18 @@
+//src/core/errors/ApiError.js 
+class ApiError extends Error {
+    constructor(
+        message: "Internal Server Error",
+        statusCode = 500,
+        errors = []
+    ) {
+        supper(message);
+
+        this.name = this.constructor.name;
+
+        this.statusCode = statusCode;
+
+        this.success = false;
+
+        this.errors = errors;
+
+        Error.captureStackTrace(this, this.con)
