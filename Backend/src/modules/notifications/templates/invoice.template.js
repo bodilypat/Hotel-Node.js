bdd57bdd794a8c8a773770945298f80dcfe273fa
bodilypat <139784676+bodilypat@users.noreply.github.com>
2026-05-11 06:23:12 -0700

    Create invoice.template.js

---
+++ Backend/src/modules/notifications/templates/invoice.template.js
@@ -0,0 +1,22 @@
+//src/modules/notifications/templates/invoice.template.js 
+const invoiceTemplate = ({
+    name,
+    invoiceId,
+    amount,
+}) => {
+    return `
+        <div>
+            <h2>Invoice</h2>
+            
+            <p>Hello ${name},</p>
+            
+            <p>Your invoice has been generated.</p>
+            
+            <ul>
+                <li>Invoice Id: ${invoiced}</li>
+                <li>Total Amount: ${amount}</li>
+            </ul>
+        `;
+};
+
+export default invoiceTemplate;
