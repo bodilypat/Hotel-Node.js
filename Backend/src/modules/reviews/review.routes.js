1edaab3efa74ea5d90503c0f2b7b2b05636e3376
bodilypat <139784676+bodilypat@users.noreply.github.com>
2026-05-11 06:18:46 -0700

    Add review routes for CRUD operations

---
+++ Backend/src/modules/reviews/review.routes.js
@@ -0,0 +1,33 @@
+//src/modules/reviews/review.routes.js 
+import express from "express";
+
+import {
+    createReview,
+    getReviews,
+    getReview,
+    getHotelReviews,
+    updateReview,
+    deleteReview,
+} from "./review.controller.js";
+
+import authMiddleware from "../../core/middleware/auth.middleware.js";
+
+const router = express.Router();
+
+router.post(
+    "/",
+    authMiddleware,
+    createReview
+);
+
+router.get("/", getReviews);
+
+router.get("/:id", getReview);
+
+router.get(
+    "/hotel/:hotelId",
+    authMiddleware,
+    deleteReview
+);
+
+export default router;
