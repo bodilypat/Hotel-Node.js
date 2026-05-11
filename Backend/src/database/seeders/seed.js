3a179c18cda47a7f842dc84c1fdb6a8be696fcbe
bodilypat <139784676+bodilypat@users.noreply.github.com>
2026-05-11 06:30:24 -0700

    Create seed.js

---
+++ Backend/src/database/seeders/seed.js
@@ -0,0 +1,27 @@
+//src/database/seeders/seed.js 
+import mongoose from "mongoose";
+import env from "../../config/env.js";
+
+import seedUsers from "./users.seeder.js";
+import seedHotels from "./hotels.seeder.js";
+
+const seedDatabase = async () => {
+    try {
+        await mongoose.connect(env.MONGO_URI);
+
+        console.log("Database connected");
+
+        await seedUsers();
+        await seedHotels();
+
+        console.log("Database seeded successfully");
+
+        process.exit(0);
+    } catch (error) {
+        console.error(error);
+
+        process.exit(1);
+    }
+};
+
+seedDatabase();
