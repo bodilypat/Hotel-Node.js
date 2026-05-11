eba5a1a5aa683c9ad99fdb08467a2194f64bc084
bodilypat <139784676+bodilypat@users.noreply.github.com>
2026-05-11 05:56:31 -0700

    Create asyncHandler.js

---
+++ Backend/src/core/utils/asyncHandler.js
@@ -0,0 +1,8 @@
+//src/core/utils/asyncHandler.js 
+const asyncHandler = (fn) => {
+    return (req, res, next) => {
+        Promise.resolve(fn(req, res, next)).catch(next);
+    };
+};
+
+export default asyncHandler;
