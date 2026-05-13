bd512f39f0d9fd78fb0d04006426f01070c96e3f
bodilypat <139784676+bodilypat@users.noreply.github.com>
2026-05-13 06:50:30 -0700

    Create login.validation.js

---
+++ Backend/src/modules/auth/validations/login.validation.js
@@ -0,0 +1,13 @@
+//src/modules/auth/validations/login.validation.js 
+const Joi = require('joi');
+
+module.exports = Joi.object({
+    body: Joi.object({
+        email: Joi.string()
+          .email()
+          .required(),
+        password: Joi.string()
+          .min(6)
+          .required(),
+    }),
+});
