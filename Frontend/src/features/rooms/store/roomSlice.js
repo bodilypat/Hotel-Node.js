a9bfd12682b14c86e9415c1ffd0306952beff00b
bodilypat <139784676+bodilypat@users.noreply.github.com>
2026-06-25 13:07:27 -0700

    Add files via upload

--- Frontend/src/features/rooms/store/roomSlice.js
+++ Frontend/src/features/rooms/store/roomSlice.js
@@ -1,1 +1,41 @@
-//src/features/rooms/store/roomSlice.js
+//src/features/rooms/store/roomSlice.js 
+
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import * as roomApi from "./roomApi";
+
+export const fetchRooms = createAsyncThunk(
+    "rooms/fetchRooms",
+    async () => {
+        return await roomApi.getRoom();
+    }
+);
+
+const roomSlice = createSlice({
+    name: "rooms",
+    initialState: {
+        rooms: [],
+        loading: false,
+        error: null,
+    },
+
+    reducers: {},
+    extraReducers: (builder) => {
+        builder 
+            .addCase(fetchRooms.pending, (state) => {
+                state.loading = true;
+            })
+            .addCase(fetchRooms.fulfilled, (state, action) => {
+                state.loading = false;
+                state.rooms  = action.payload;
+            })
+            .addCase(fetchRooms.rejected, (state, action) => {
+                state.loading = false;
+                state.error = action.error.message;
+            });
+    },
+});
+
+export default roomSlice.reducer;
+
+
+
